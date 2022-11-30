d5fe902345b3f0007dc2f83c92f540311eaa1ae6
mbocciarelli <bocciarellimathieu@orange.fr>
2022-11-30 22:31:39 +0100

    Initialisation des fichiers/dossiers

--- scripts/linux-build-release.sh
+++ scripts/linux-build-release.sh
@@ -1,3 +1,4 @@
+cd ..
 mkdir build-release && cd build-release || exit
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=.. -DBUILD_SHARED_LIBS=ON
 make
--- scripts/windows-build-debug.bat
+++ scripts/windows-build-debug.bat
@@ -1,3 +1,4 @@
+cd ..
 mkdir build-debug && cd build-debug
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=.. -DBUILD_SHARED_LIBS=ON
 cd ..
